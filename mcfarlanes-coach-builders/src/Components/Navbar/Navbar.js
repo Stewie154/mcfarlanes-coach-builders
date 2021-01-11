eb7e85b41c17e61ec44d3c34109f2750e95fc2c3
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-11 15:31:00 +0000

    Start of Navbar

---
+++ mcfarlanes-coach-builders/src/Components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Styles/Navbar.css';
+import {GiHamburgerMenu} from 'react-icons/gi';
+
+const Navbar = () => {
+    return (
+        <div className="navbar-container">
+            <div className="logo">
+                <h2>McFarlane's Coach Builders</h2>
+            </div>
+            <div className="nav-links-container">
+                <p className="nav-link">
+                    About
+                </p>
+                <p className="nav-link">
+                    Previous Work
+                </p>
+                <p className="nav-link">
+                    Contact
+                </p>
+            </div>
+            <GiHamburgerMenu className="menu-icon"/>
+        </div>
+    )
+}
+
+export default Navbar
