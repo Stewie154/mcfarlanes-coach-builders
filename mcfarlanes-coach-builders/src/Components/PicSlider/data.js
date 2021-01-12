663b28b29dd7d8c9a01c05566136ec0f1ea1bd42
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-12 16:28:26 +0000

    Started Image Slider
    Here I have imported everything necessary for the image slider in the Hero Section. I'll continue to work on this is the next commit.

---
+++ mcfarlanes-coach-builders/src/Components/PicSlider/data.js
@@ -0,0 +1,23 @@
+import car1 from '../../Images/Car1.jpg';
+import car2 from '../../Images/Car2.jpg';
+import car3 from '../../Images/Car3.jpg';
+import car4 from '../../Images/Car4.jpg';
+
+export const data = [
+    {
+        name: "Pic 1",
+        src: car1
+    },
+    {
+        name: "Pic 2",
+        src: car2
+    },
+    {
+        name: "Pic 3",
+        src: car3
+    },
+    {
+        name: "Pic 4",
+        src: car4
+    }
+]
