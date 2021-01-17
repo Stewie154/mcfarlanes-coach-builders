9a41d49b94f214354d573424501f474df87f7857
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-17 10:52:47 +0000

    PreviousWork toggleBigPic
    Here I've updated the toggleBigPic function to target the picture that was clicked. So now when a picture is clicked, an enlarged version of that picture is displayed on the screen.
    
    I did this by giving each job object 2 more properties, those being a boolean of showingEnlarged and unique key of a number. I gave the component some more state of [jobs, setJobs] and initialized it with the previousWorkData. I got rid of the setBigPic state as the showingEnlarged property did the same thing.
    
    In the toggleBigPic function I mapped through jobs from the state and compared the id properties with the index of the job that was clicked. If a match is found that the showingEnlarged property for that job is toggled.
    
    I used a ternary operator on the large image and the close icon, so when the showingEnlarged property for that job is set to true, the image and icon are displayed to the user. When it's false the class of 'hide' is applied removing the items from the screen.

--- mcfarlanes-coach-builders/src/Components/PreviousWork/PreviousWork.js
+++ mcfarlanes-coach-builders/src/Components/PreviousWork/PreviousWork.js
@@ -4,10 +4,19 @@ import { FaTimesCircle } from 'react-icons/fa';
 import { prevWorkData } from './PreviousWorkData';
 const PreviousWork = () => {
 
-    const [bigPic, setBigPic] = useState(false);
+    
 
-    const toggleBigPic = () => {
-        setBigPic(!bigPic);
+    const [jobs, setJobs] = useState(prevWorkData);
+
+    const toggleBigPic = (index) => {
+
+        setJobs(prevState => (
+            prevState.map( (job) => {
+                return index == job.id ? {...job, showingEnlarged: !job.showingEnlarged } : job
+            }
+        )
+        ));
+        
     }
 
     return (
@@ -17,14 +26,14 @@ const PreviousWork = () => {
                 <p>Feel free to take a look at some of Stephen's previous work below.</p>
             </div>
             <div className="prev-work-container">
-                {prevWorkData.map((job, index) => {
+                {jobs.map((job, index) => {
                     return (
                         <div className="job" key={index}>
-                        <img src={job.src} alt="Classic Car" className="small-pic" onClick={toggleBigPic}/>
+                        <img src={job.src} alt="Classic Car" className="small-pic" onClick={() => toggleBigPic(index)}/>
                         <h2>{job.title}</h2>
                         <p>{job.desc}</p>
-                        <FaTimesCircle className={ bigPic ? "display-icon" : "hide" } onClick={toggleBigPic}/>
-                        <img src={job.src} alt="Classic Car" className={ bigPic ? "display-big-pic" : "hide" }/>
+                        <FaTimesCircle className={ job.showingEnlarged ? "display-icon" : "hide" } onClick={() => toggleBigPic(index)}/>
+                        <img src={job.src} alt="Classic Car" className={ job.showingEnlarged ? "display-big-pic" : "hide" }/>
                         </div>
                     )
                 } )}
