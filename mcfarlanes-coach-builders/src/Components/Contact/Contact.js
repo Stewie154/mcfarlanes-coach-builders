669389c44a5f74fafd1b1092e59f3111565c78a9
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-18 11:41:57 +0000

    Contact component finsihed
    Here I've fnished the Contact section, making it mobile responsive.

--- mcfarlanes-coach-builders/src/Components/Contact/Contact.js
+++ mcfarlanes-coach-builders/src/Components/Contact/Contact.js
@@ -9,9 +9,15 @@ const Contact = () => {
             <h1>Contact</h1>
             <div className="map-container">
                 <div className="info-box">
-                    <p className="inquiries">For inquiries, please contact Stephen McFarlane via telephone or email.</p>
+                    <p className="inquiries">For inquiries, please contact Steve McFarlane via telephone or email.</p>
+                    <br/>
+                    <a href="tel:07778593651"><i>07778 593651</i></a>
+                    <br/>
+                    <a href="mailto:stevemcfarlane@btinternet.com" rel="noreferrer" target="_blank">
+                        <i id="primary-email">stevemcfarlane@btinternet.com</i>
+                    </a>
                 </div>
-                <div className="info-box">
+                <div className="info-box" >
                     <h2>Where to find us:</h2>
                     <p>The Nook,</p>
                     <p>Bentley Road,</p>
@@ -31,15 +37,21 @@ const Contact = () => {
                 </div>
             </div>
             <div className="contact-info-container">
-                <a href="mailto:stevemcfarlane@btinternet.com" rel="noreferrer" target="_blank"><p>stevemcfarlane@btinternet.com</p></a>
-                <AiOutlineLinkedin className="linked-in" />
+                <a href="mailto:stevemcfarlane@btinternet.com" rel="noreferrer" target="_blank">
+                    <p className="email">stevemcfarlane@btinternet.com</p>
+                </a>
+                <a href="https://www.stewartmcfarlane.me/" target="_blank" rel="noreferrer" >
+                    <AiOutlineLinkedin className="linked-in" />
+                </a>
                 <div className="phone-numbers-container">
                     <p>+44(0) 1206 250239 (Home)</p>
                     <p>+44(0) 7778 593651 (Mobile)</p>
                 </div>
                 
             </div>
-            <p className="plug">Website by Stewart McFarlane</p>
+            <a href="https://www.stewartmcfarlane.me/" target="_blank" rel="noreferrer" >
+                <p className="plug">Website by Stewart McFarlane</p>
+            </a>
         </div>
     )
 }
