8a47709d9c9e5b2340405654782e837dec2bd21b
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-22 17:23:05 +0000

    Added prictures to PreviousWork
    Here I've replaced the stock pictures in the PreviousWork component with the real ones. I compressed them before adding them.

--- mcfarlanes-coach-builders/src/Components/PreviousWork/PreviousWorkData.js
+++ mcfarlanes-coach-builders/src/Components/PreviousWork/PreviousWorkData.js
@@ -1,6 +1,24 @@
-import pic1 from '../../Images/Car1.jpg';
-import pic2 from '../../Images/Car2.jpg';
-import pic3 from '../../Images/Car3.jpg';
+import pic1 from '../../Images/Previous-Work-Photos/Previous-Work-1.jpg';
+import pic2 from '../../Images/Previous-Work-Photos/Previous-Work-2.jpg';
+import pic3 from '../../Images/Previous-Work-Photos/Previous-Work-3.jpg';
+import pic4 from '../../Images/Previous-Work-Photos/Previous-Work-4.jpg';
+import pic5 from '../../Images/Previous-Work-Photos/Previous-Work-5.jpg';
+import pic6 from '../../Images/Previous-Work-Photos/Previous-Work-6.jpg';
+import pic7 from '../../Images/Previous-Work-Photos/Previous-Work-7.jpg';
+import pic8 from '../../Images/Previous-Work-Photos/Previous-Work-8.jpg';
+import pic9 from '../../Images/Previous-Work-Photos/Previous-Work-9.jpg';
+import pic10 from '../../Images/Previous-Work-Photos/Previous-Work-10.jpg';
+import pic11 from '../../Images/Previous-Work-Photos/Previous-Work-11.jpg';
+import pic12 from '../../Images/Previous-Work-Photos/Previous-Work-12.jpg';
+import pic13 from '../../Images/Previous-Work-Photos/Previous-Work-13.jpg';
+import pic14 from '../../Images/Previous-Work-Photos/Previous-Work-14.jpg';
+import pic15 from '../../Images/Previous-Work-Photos/Previous-Work-15.jpg';
+import pic16 from '../../Images/Previous-Work-Photos/Previous-Work-16.jpg';
+import pic17 from '../../Images/Previous-Work-Photos/Previous-Work-17.jpg';
+import pic18 from '../../Images/Previous-Work-Photos/Previous-Work-18.jpg';
+import pic19 from '../../Images/Previous-Work-Photos/Previous-Work-19.jpg';
+import pic20 from '../../Images/Previous-Work-Photos/Previous-Work-20.jpg';
+
 
 
 export const prevWorkData = [
@@ -26,25 +44,123 @@ export const prevWorkData = [
         id: 2
     },
     {
-        src: pic1,
+        src: pic4,
         title: 'Enzo Ferrari 1970',
         desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
         showingEnlarged: false,
         id: 3
     },
     {
-        src: pic1,
+        src: pic5,
         title: 'Enzo Ferrari 1970',
         desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
         showingEnlarged: false,
         id: 4
     },
     {
-        src: pic1,
+        src: pic6,
         title: 'Enzo Ferrari 1970',
         desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
         showingEnlarged: false,
         id: 5
+    },
+    {
+        src: pic7,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 6
+    },
+    {
+        src: pic8,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 7
+    },
+    {
+        src: pic9,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 8
+    },
+    {
+        src: pic10,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 9
+    },
+    {
+        src: pic11,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 10
+    },
+    {
+        src: pic12,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 11
+    },
+    {
+        src: pic13,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 12
+    },
+    {
+        src: pic14,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 13
+    },
+    {
+        src: pic15,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 14
+    },
+    {
+        src: pic16,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 15
+    },
+    {
+        src: pic17,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 16
+    },
+    {
+        src: pic18,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 17
+    },
+    {
+        src: pic19,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 18
+    },
+    {
+        src: pic20,
+        title: 'Enzo Ferrari 1970',
+        desc: 'Here will be a description for each individual car. It will probably be about two sentences long.',
+        showingEnlarged: false,
+        id: 19
     },
     
 ]
