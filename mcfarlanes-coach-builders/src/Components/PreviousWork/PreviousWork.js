fdf881f1de914306cb72fca79a89701062fb3e2d
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-27 13:56:48 +0000

    Bug Fixes
    Here I've changed the container .info-box to have a min-height of 40vh. Before this change, some of the text went outside of it's container on small screens.
    
    I've also changed the display for the big picture in the PreviousWork component. I've moved the x icon and big pic into a container (big-pic-container) and have centered the container to the screen. This solved the problem of the x icon and big pic moving on different screen sizes. Now everything is centered on every screen size, with the x icon in the top right corner of the big picture.
    
    I've also given various elements different z indexes, as some elements were overlapping in the wrong places. This has now been fixed.

--- mcfarlanes-coach-builders/src/Components/PreviousWork/PreviousWork.js
+++ mcfarlanes-coach-builders/src/Components/PreviousWork/PreviousWork.js
@@ -31,8 +31,11 @@ const PreviousWork = () => {
                         <div className="job" key={index}>
                             <img src={job.src} alt="Classic Car" className="small-pic" onClick={() => toggleBigPic(index)}/>
                             <h2>{job.title}</h2>
-                            <FaTimesCircle className={ job.showingEnlarged ? "display-icon" : "hide" } onClick={() => toggleBigPic(index)}/>
-                            <img src={job.src} alt="Classic Car" className={ job.showingEnlarged ? "display-big-pic" : "hide" }/>
+                            <div className="big-pic-container" className={ job.showingEnlarged ? "big-pic-container" : "hide" }>
+                                <FaTimesCircle className={ job.showingEnlarged ? "display-icon" : "hide" } onClick={() => toggleBigPic(index)}/>
+                                <img src={job.src} alt="Classic Car" className="big-pic"/>
+                            </div>
+                            
                         </div>
                     )
                 } )}
